b34ef9655f4f26240b7b906f66908045169df2cb
Bijay Adhikari <bijay.adhikari@info.com.np>
2024-02-06 14:26:51 +0545

    added dbset and modelbuilder and model class users

---
+++ EcommerceMVC/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceMVC.Models
+{
+    public class Users
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public DateTime CreatedDate { get; set; }
+        public bool Isactive { get; set; }
+
+        public string Password { get; set; }
+    }
+}
--- EcommerceMVC/data/ApplicationDbContext.cs
+++ EcommerceMVC/data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EcommerceMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceMVC.data
 {
@@ -12,32 +13,27 @@ namespace EcommerceMVC.data
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ConnectionStrings"));
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
         }
 
-      /*  public DbSet<Product> Products { get; set; }
+        public DbSet<Users> Users { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    // Replace "your_connection_string_here" with your actual connection string
-        //    optionsBuilder.UseSqlServer("your_connection_string_here");
-        //}
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Configure the Product entity
-            modelBuilder.Entity<Product>(entity =>
+            modelBuilder.Entity<Users>(x =>
             {
-                entity.ToTable("Products"); // Set the table name
-                entity.HasKey(e => e.Id); // Set the primary key
-                entity.Property(e => e.Name).IsRequired(); // Set Name as required
-                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)"); // Set Price as decimal(18, 2)
+                x.ToTable("Users"); 
+                x.HasKey(e => e.Id);
+                x.Property(e => e.Name).IsRequired(); 
+                x.Property(e => e.Password).IsRequired();
+                x.Property(e => e.Isactive).HasDefaultValue(true);
+
             });
 
-            // Add more configurations for other entities if needed
+           
 
             base.OnModelCreating(modelBuilder);
-        }*/
+        }
 
     }
 }
